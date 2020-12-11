86a7df6eb2b509c6cb839dc81470244e3e0e7bd3
Dylan Waters <dylan.waters1@hotmail.com>
2020-12-11 12:38:48 +0000

    Added a working login system

---
+++ src/java/Pages/Login.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Pages;
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.lang.NullPointerException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import model.Jdbc;
+
+/**
+ *
+ * @author Dylan
+ */
+public class Login extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     * @throws java.sql.SQLException
+     */
+      protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, SQLException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        HttpSession session = request.getSession(false);
+        
+        String [] query = new String[2];
+        query[0] = (String)request.getParameter("username");
+        query[1] = (String)request.getParameter("password");
+      
+        Jdbc jdbc = (Jdbc)session.getAttribute("dbbean"); 
+        
+        if (jdbc == null){
+            request.getRequestDispatcher("/WEB-INF/conErr.jsp").forward(request, response);
+        }
+        if(query[0].equals("") ) {
+            request.setAttribute("message", "Username cannot be NULL");
+        } 
+        if(jdbc.login(query[0],query[1])){
+            /*String job = jdbc.retriveType(query[0]);
+            String destination = jobs(job);
+            request.getRequestDispatcher(destination).forward(request, response);*/
+            request.setAttribute("message", query[0]+" Welcome back");
+          
+        }else{
+            request.setAttribute("message", query[0]+ " Username/Password not found");
+        }
+
+         
+        request.getRequestDispatcher("/WEB-INF/user.jsp").forward(request, response);
+    }
+      
+      public String jobs(String string){
+          String dest = null;
+          if(string.equals("doctor")){
+               dest = "/WEB-INF/conErr.jsp";
+          }else if(string.equals("nurse")){
+               dest = "/WEB-INF/conErr.jsp";
+          }else if(string.equals("admin")){
+               dest = "/WEB-INF/conErr.jsp";
+          }else if(string.equals("client")){
+               dest = "/WEB-INF/conErr.jsp";
+          }
+          return dest;
+    }
+        
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+          try {
+              processRequest(request, response);
+          } catch (SQLException ex) {
+              Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+          }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+          try {
+              processRequest(request, response);
+          } catch (SQLException ex) {
+              Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+          }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
+
---
+++ src/java/model/Jdbc.java
@@ -0,0 +1,229 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Time;
+import static java.sql.Types.NULL;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author me-aydin
+ */
+public class Jdbc {
+    
+    Connection connection = null;
+    Statement statement = null;
+    ResultSet rs = null;
+    //String query = null;
+    
+    
+    public Jdbc(String query){
+        //this.query = query;
+    }
+
+    public Jdbc() {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public void connect(Connection con){
+       connection = con;
+    }
+    
+    private ArrayList rsToList() throws SQLException {
+        ArrayList aList = new ArrayList();
+
+        int cols = rs.getMetaData().getColumnCount();
+        while (rs.next()) { 
+          String[] s = new String[cols];
+          for (int i = 1; i <= cols; i++) {
+            s[i-1] = rs.getString(i);
+          } 
+          aList.add(s);
+        } // while    
+        return aList;
+    } //rsToList
+ 
+    private String makeTable(ArrayList list) {
+        StringBuilder b = new StringBuilder();
+        String[] row;
+        b.append("<table border=\"3\">");
+        for (Object s : list) {
+          b.append("<tr>");
+          row = (String[]) s;
+            for (String row1 : row) {
+                b.append("<td>");
+                b.append(row1);
+                b.append("</td>");
+            }
+          b.append("</tr>\n");
+        } // for
+        b.append("</table>");
+        return b.toString();
+    }//makeHtmlTable
+    
+    private void select(String query){
+        //Statement statement = null;
+        
+        try {
+            statement = connection.createStatement();
+            rs = statement.executeQuery(query);
+            //statement.close();
+        }
+        catch(SQLException e) {
+            System.out.println("way way"+e);
+            //results = e.toString();
+        }
+    }
+  /*  public String retrieve(String user) throws SQLException{
+        
+        StringBuilder sb = new StringBuilder();
+        select("select JOB from users where UNAME='"+user.trim()+"'");
+        
+        if (rs==null)
+            System.out.println("rs is null");
+        else
+            rsToList();
+        
+        
+        return rsToList();//results;
+    }*/
+    public String retriveType(String user) throws SQLException {
+    String type = "";
+    select("select JOB from users where UNAME='"+user.trim()+"'");
+     int cols = rs.getMetaData().getColumnCount();
+    while (rs.next()) {
+        String[] s = new String[cols];
+        for (int i = 1; i <= cols; i++) {
+            s[i-1] = rs.getString(i);
+        }
+        type = s[cols-1];
+    } // while
+    return type;
+    }
+    
+    public boolean exists(String user) {
+        boolean bool = false;
+        try  {
+            select("select UNAME from users where UNAME='"+user+"'");
+            if(rs.next()) {
+                System.out.println("TRUE");         
+                bool = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return bool;
+    }
+    
+    public boolean login(String user, String password){
+        boolean bool = false;
+        try{
+            select("select uname from users where UNAME='"+user+"'" +" and PASSWD='" +password+"'");
+            if(rs.next()){
+                System.out.println("Login Successfull");
+                bool = true;
+                }
+            }catch(SQLException ex) {
+            Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       return bool; 
+    }
+    public void insert(String[] str){
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement("INSERT INTO Users VALUES (?,?,?)",PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, str[0].trim()); 
+            ps.setString(2, str[1].trim());
+            ps.setString(3, str[2]);
+            ps.executeUpdate();
+        
+            ps.close();
+            System.out.println("1 row added.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         
+    }
+    public void update(String[] str) {
+        PreparedStatement ps = null;
+        try {
+            ps = connection.prepareStatement("Update Users Set passwd=? where uname=?",PreparedStatement.RETURN_GENERATED_KEYS);
+            ps.setString(1, str[1].trim()); 
+            ps.setString(2, str[0].trim());
+            ps.executeUpdate();
+        
+            ps.close();
+            System.out.println("1 rows updated.");
+        } catch (SQLException ex) {
+            Logger.getLogger(Jdbc.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void delete(String user){
+       
+      String query = "DELETE FROM Users " +
+                   "WHERE uname = '"+user.trim()+"'";
+      
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+        }
+        catch(SQLException e) {
+            System.out.println("way way"+e);
+            //results = e.toString();
+        }
+    }
+    public void closeAll(){
+        try {
+            rs.close();
+            statement.close(); 		
+            //connection.close();                                         
+        }
+        catch(SQLException e) {
+            System.out.println(e);
+        }
+    }
+    public static void main(String[] args) throws SQLException {
+        String str = "select * from users";
+        String insert = "INSERT INTO `Users` (`uname`, `passwd`, 'job') VALUES ('meaydin', 'meaydin','doctor')";
+        String update = "UPDATE `Users` SET `passwd`='eaydin' WHERE `uname`='meaydin' ";
+        String db = "MyDB";
+        
+        Jdbc jdbc = new Jdbc(str);
+        Connection conn = null;
+                try {
+            Class.forName("org.apache.derby.jdbc.ClientDriver");
+//Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/myNewDB","dylan","123456");
+            conn = DriverManager.getConnection("jdbc:derby://localhost:1527/"+db.trim(),"dylan","123456");
+        }
+        catch(ClassNotFoundException | SQLException e){
+            
+        }
+        jdbc.connect(conn);
+        String [] users = {"eaydin","benim","benim"};
+        //System.out.println(jdbc.retrieveType(str));
+        if (!jdbc.exists(users[0]))
+            jdbc.insert(users);            
+        else {
+                jdbc.update(users);
+                System.out.println("user name exists, change to another");
+        }
+        //jdbc.delete("aydinme");
+        System.out.println(jdbc.retriveType(str));
+        jdbc.closeAll();
+    }            
+}
